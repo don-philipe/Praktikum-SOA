d991e2772af2e8d7e7286ed9dda33c9b5a96c977
Tobias Hafermalz <t.hafermalz@gmail.com>
2013-12-16 16:45:09 +0100

    - HTTP Status Codes hinzugefügt

--- src/api/index.php
+++ src/api/index.php
@@ -17,6 +17,7 @@ $app->get('/bikes', function () use ($app) {
 	$location = $app->request()->get('location'); 
 	$radius = $app->request()->get('radius'); 
 	if($radius == '') $radius = 2500;
+	if($location == '') $app->halt(400, 'Parameter missing!');
 	
 	$coordinates = getCoordinates($location);
 	$lat = $coordinates->lat;
@@ -32,14 +33,14 @@ $app->get('/bikes', function () use ($app) {
 	//limit bikes to selected distance
 	$query .=  " HAVING distance < $radius ORDER BY distance";
     
-    echo getJsonObjectsFromDb($query);  
+    echo getJsonObjectsFromDb($query, $app);  
 });
 
 
-$app->get('/bikes/:bikeId', function ($id) {
+$app->get('/bikes/:bikeId', function ($id) use ($app) {
 	$query = "SELECT id, model, price, longitude, latitude, station FROM bikes WHERE id = $id";
     
-    echo getJsonObjectFromDb($query); 
+    echo getJsonObjectFromDb($query, $app); 
 });
 
 
@@ -56,13 +57,13 @@ $app->get('/models', function () use ($app) {
 	
 	$query .= ' GROUP BY models.id';
     
-    echo getJsonObjectsFromDb($query);  
+    echo getJsonObjectsFromDb($query, $app);  
 });
 
 
-$app->get('/models/:modelId', function ($id) {
-	$query = "SELECT* FROM models WHERE id = $id";
-    echo getJsonObjectFromDb($query);  
+$app->get('/models/:modelId', function ($id) use ($app) {
+	$query = "SELECT * FROM models WHERE id = $id";
+    echo getJsonObjectFromDb($query, $app);  
 });
 
 
@@ -79,42 +80,45 @@ $app->get('/stations', function () use ($app) {
 	
 	$query .= ' GROUP BY stations.id';
 
-    echo getJsonObjectsFromDb($query);
+    echo getJsonObjectsFromDb($query, $app);
 });
 
 
-$app->get('/stations/:stationId', function ($id) {
+$app->get('/stations/:stationId', function ($id) use ($app) {
 	$query = "SELECT * FROM stations WHERE id = $id";
-    echo getJsonObjectFromDb($query);
+    echo getJsonObjectFromDb($query, $app);
 });
 
 
-$app->get('/account', function () {
+$app->get('/account', function () use ($app) {
 	$query = "SELECT id, email FROM accounts";
-    echo getJsonObjectFromDb($query);
+    echo getJsonObjectFromDb($query, $app);
 });
 
 
-$app->get('/bookings', function () {
+$app->get('/bookings', function () use ($app) {
 	$query = "SELECT id, bike, date FROM bookings";
-    echo getJsonObjectsFromDb($query);
+    echo getJsonObjectsFromDb($query, $app);
 });
 
 
-$app->get('/bookings/:bookingId', function ($id) {
+$app->get('/bookings/:bookingId', function ($id) use ($app) {
 	$query = "SELECT id, bike, date FROM bookings WHERE id = $id";
-    echo getJsonObjectFromDb($query);
+    echo getJsonObjectFromDb($query, $app);
 });
 
 
 $app->post('/bookings', function() use($app) {
-	$bikeId = $app->request()->get('bikeId');
+	$bikeId = $app->request()->params('bikeId');
+	if(!$bikeId) $app->halt(400, 'Parameter missing!');
+	
 	$postQuery = "INSERT INTO bookings (bike, account) VALUES ($bikeId, 1)";
 	
-	$id = addAndGetIdFromDb($postQuery);
-        
+	$id = addAndGetIdFromDb($postQuery, $app);
+                
     $query = "SELECT id, bike, date FROM bookings WHERE id = $id";
-    echo getJsonObjectFromDb($query);
+    echo getJsonObjectFromDb($query, $app);
+    $app->response()->status(201);
 });
 
 
@@ -122,60 +126,69 @@ $app->put('/bookings/:bookingId', function($id) use($app) {
 	$bikeId = $app->request()->params('bikeId');
 	
 	$putQuery = "UPDATE bookings SET bike = $bikeId WHERE id = $id";
-	updateDb($putQuery);
+	updateDb($putQuery, $app);
         
     $query = "SELECT id, bike, date FROM bookings WHERE id = $id";
-    echo getJsonObjectFromDb($query);
+    echo getJsonObjectFromDb($query, $app);
 });
 
 
 $app->delete('/bookings/:bookingId', function($id) use($app) {
 	$query = "DELETE FROM bookings WHERE id = $id";
-	deleteRowInDb($query);
+	deleteRowInDb($query, $app);
 });
 
 $app->run();
 
 
 //Connects to db and returns a single result for the given query
-function getJsonObjectFromDb($query){
+function getJsonObjectFromDb($query, $app){
 	$dbcon = getConnection();
     $stmt = $dbcon->query($query);
     $object = $stmt->fetchObject();   
     $dbcon = null;
+    
+    if(!$object) $app->halt(404);
     return json_encode($object);    
 }
 
 //Connects to db and returns an array of results for the given query
-function getJsonObjectsFromDb($query){
+function getJsonObjectsFromDb($query, $app){
 	$dbcon = getConnection();
     $stmt = $dbcon->query($query);
     $objects = $stmt->fetchAll(PDO::FETCH_ASSOC);   
     $dbcon = null;
+    
     return json_encode($objects);    
 }
 
 //Connects to db, adds a row and returns the id
-function addAndGetIdFromDb($query){
+function addAndGetIdFromDb($query, $app){
 	$dbcon = getConnection();
     $stmt = $dbcon->query($query);
     $id = $dbcon->lastInsertId();
     $dbcon = null;  
+    
+    if(!$id) $app->halt(400);
+    
     return $id;
 }
 
 //Connects to db and updates row
-function updateDb($query){
+function updateDb($query, $app){
 	$dbcon = getConnection();
     $stmt = $dbcon->query($query);
     $dbcon = null;
 }
 
 //Connects to db and delets a row
-function deleteRowInDb($query){
+function deleteRowInDb($query, $app){
 	$dbcon = getConnection();
     $stmt = $dbcon->query($query);
     $dbcon = null;  
+    
+    //$app->halt(204);
+    $app->response()->status(204);
 }
 
 // Return a connection to the database using PHP PDO
