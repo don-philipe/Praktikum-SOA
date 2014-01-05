13f739f36955387780831a4c92413338bac90431
don-philipe <don_philipe@gmx.de>
2014-01-05 23:40:11 +0100

    problems with downloaded jquery

--- src/header.php
+++ src/header.php
@@ -4,6 +4,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link href="css/style.css" type="text/css" rel="stylesheet">
 		<script language="javascript" type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+		<!--<script language="javascript" type="text/javascript" src="js/lib/jquery-2.0.3.min.js"></script>-->
 		<script language="javascript" type="text/javascript" src="js/script.js"></script>
 		
 		<title>BikeSharing</title>
@@ -20,7 +21,7 @@
 						<li><a href="search.php">Suche</a></li>
 						<li><a href="stations.php">Standorte</a></li>
 						<li><a href="models.php">Fahrradmodelle</a></li>
-						<li><a href="contact.php">Kontakt</a></li>
+						<!--<li><a href="contact.php">Kontakt</a></li>-->
 					</ul>
 				</nav>
 				
