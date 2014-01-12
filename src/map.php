2830b3b37b6c6cd8fdc00a39b41cad85707d9c65
don-philipe <don_philipe@gmx.de>
2014-01-12 17:47:01 +0100

    now with map

---
+++ src/map.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<script type="text/javascript" src="http://www.openlayers.org/api/OpenLayers.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+<body onload="initMap();">
+	<div id="basicMap"></div>
+</body>
+</html>
--- src/stations.php
+++ src/stations.php
@@ -4,7 +4,6 @@ include_once('header.php')
 
 	<script type="text/javascript">
 		showStations();
-		initMap();
 	</script>
 			
 	<section>
@@ -12,13 +11,9 @@ include_once('header.php')
 		<article id='stations'>
 			<h2>Stationen</h2>
 			<!--TODO: insert OSM map and get gps from db-->
-			<div id="basicMap"></div>
 		</article>
 		
 	</section>
-			
-			
-		
 		
 <?php
 include_once('footer.php')
